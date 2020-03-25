89baab1f621ad92695ac574b959284006106ea5b
Nick Richmond <nwrichmond@turnt.tech>
2020-03-25 17:42:12 -0400

    Add storybook config and register add-ons

---
+++ .storybook/config.js
@@ -0,0 +1,28 @@
+import { addParameters, configure } from '@storybook/vue'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import '../assets/base-styles.css'
+
+import nacellePlugin from '../tests/mocks/nacelle-vue-plugin'
+import nacelleHelpers from '../tests/mocks/nacelle-helpers'
+import axiosModule from '../tests/mocks/axios-module'
+
+Vue.use(Vuex)
+Vue.use(nacellePlugin)
+Vue.use(nacelleHelpers)
+Vue.use(axiosModule)
+
+const req = require.context('../components/stories', true, /.stories.js$/)
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename))
+}
+
+configure(loadStories, module)
+
+addParameters({
+  options: {
+    showPanel: true,
+    panelPosition: 'right'
+  }
+})
