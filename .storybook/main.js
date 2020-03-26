63b3129192e8858272b40134e36019ca98bdb9d0
Nick Richmond <nwrichmond@turnt.tech>
2020-03-26 19:13:13 -0400

    Replace external webpack config with webpackFinal
    This is the canonical way to extend the webpack config in the current version of storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,18 @@
+const path = require('path');
+
 module.exports = {
-  stories: ['../components/stories/*.stories.[tj]s']
+  stories: ['../components/stories/*.stories.[tj]s'],
+  webpackFinal: async (config, resolve) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+    
+    resolve.alias = {
+      '@': path.dirname(path.resolve(__dirname))
+    }
+
+    return config;
+  },
 }
