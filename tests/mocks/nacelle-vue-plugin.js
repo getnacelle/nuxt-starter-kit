23578598d404421149c3333d8bf5f745bd3af7b7
Andrew Laskey <andrew.laskey@gmail.com>
2020-03-27 11:05:23 -0700

    fix $nacelle jest mock

--- tests/mocks/nacelle-vue-plugin.js
+++ tests/mocks/nacelle-vue-plugin.js
@@ -5,37 +5,39 @@ export default {
   install(Vue) {
     Vue.prototype.$nacelle = {
       locale: 'en-us',
-      products({ handles }) {
-        const products = handles.map(handle => defaultProduct)
+      data: {
+        products({ handles }) {
+          const products = handles.map(handle => defaultProduct)
 
-        return new Promise(resolve => {
-          setTimeout(() => {
-            resolve(products)
-          }, 100)
-        })
-      },
-      content({ handle, type }) {
-        return new Promise(resolve => {
-          setTimeout(() => {
-            resolve({
-              id: '12345',
-              source: 'contentful',
-              type: 'content',
-              title: 'Mock Response',
-              description: 'This is a mock content response',
-              sections: defaultContent.contentful,
-              tags: [],
-              fields: {}
-            })
-          }, 100)
-        })
-      },
-      collection({ handle }) {
-        return new Promise(resolve => {
-          setTimeout(() => {
-            resolve(staticCollection)
+          return new Promise(resolve => {
+            setTimeout(() => {
+              resolve(products)
+            }, 100)
           })
-        })
+        },
+        content({ handle, type }) {
+          return new Promise(resolve => {
+            setTimeout(() => {
+              resolve({
+                id: '12345',
+                source: 'contentful',
+                type: 'content',
+                title: 'Mock Response',
+                description: 'This is a mock content response',
+                sections: defaultContent.contentful,
+                tags: [],
+                fields: {}
+              })
+            }, 100)
+          })
+        },
+        collection({ handle }) {
+          return new Promise(resolve => {
+            setTimeout(() => {
+              resolve(staticCollection)
+            })
+          })
+        }
       }
     }
   }
