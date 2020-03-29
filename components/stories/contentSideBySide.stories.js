d53ab0960875c04d1bd0a872e2cf407f13db39e9
Nick Richmond <nwrichmond@turnt.tech>
2020-03-29 05:36:02 -0400

    Uncomment story

--- components/stories/contentSideBySide.stories.js
+++ components/stories/contentSideBySide.stories.js
@@ -56,57 +56,53 @@ Default.story = {
   },
 };
 
-// export default {
-//   title: 'Components | Content / Side by Side',
-//   decorators: [withInfo, withKnobs],
-// };
+export const CustomSlots = () => ({
+  store,
+  components: { ContentSideBySide },
+  props: {
+    imageUrl: {
+      default: text('Image Url', 'https://placehold.it/800x800')
+    },
+    title: {
+      default: text('Title', 'Section Title')
+    },
+    copy: {
+      default: text('Copy', 'Lorem ipsum dolor set...')
+    },
+    ctaText: {
+      default: text('CTA Text', 'Action')
+    },
+    backgroundColor: {
+      default: text('Background Color', '')
+    },
+    reverseDesktop: {
+      default: boolean('Reverse Desktop', false)
+    },
+    reverseMobile: {
+      default: boolean('Reverse Mobile', false)
+    }
+  },
+  template: `
+    <content-side-by-side
+      :imageUrl="imageUrl"
+      :title="title"
+      :copy="copy"
+      :ctaText="ctaText"
+      :backgroundColor="backgroundColor"
+      :reverseDesktop="reverseDesktop"
+      :reverseMobile="reverseMobile"
+    >
+      <template v-slot:body>
+        <p>Custom Body Template</p>
+      </template>
+    </content-side-by-side>
+  `
+});
 
-// export const CustomSlots = () => ({
-//   components: { ContentSideBySide },
-//   props: {
-//     imageUrl: {
-//       default: text('Image Url', 'https://placehold.it/800x800')
-//     },
-//     title: {
-//       default: text('Title', 'Section Title')
-//     },
-//     copy: {
-//       default: text('Copy', 'Lorem ipsum dolor set...')
-//     },
-//     ctaText: {
-//       default: text('CTA Text', 'Action')
-//     },
-//     backgroundColor: {
-//       default: text('Background Color', '')
-//     },
-//     reverseDesktop: {
-//       default: boolean('Reverse Desktop', false)
-//     },
-//     reverseMobile: {
-//       default: boolean('Reverse Mobile', false)
-//     }
-//   },
-//   template: `
-//     <content-side-by-side
-//       :imageUrl="imageUrl"
-//       :title="title"
-//       :copy="copy"
-//       :ctaText="ctaText"
-//       :backgroundColor="backgroundColor"
-//       :reverseDesktop="reverseDesktop"
-//       :reverseMobile="reverseMobile"
-//     >
-//       <template v-slot:body>
-//         <p>Custom Body Template</p>
-//       </template>
-//     </content-side-by-side>
-//   `
-// });
-
-// CustomSlots.story = {
-//   parameters: {
-//     info: {
-//       // summary: "Hello"
-//     }
-//   },
-// };
+CustomSlots.story = {
+  parameters: {
+    info: {
+      // summary: "Hello"
+    }
+  },
+};
