790674ca4b5f2e29dd998681d62c20c3a37c54d3
curbol <curtis.bollinger@gmail.com>
2020-08-06 16:33:13 -0500

    working on fixing tests

--- tests/unit/productStore.spec.js
+++ tests/unit/productStore.spec.js
@@ -8,18 +8,20 @@ Vue.use(Vuex)
 describe('Event Store', () => {
   it('sets the product in vuex store', () => {
     const store = new Vuex.Store(storeConfig())
-    store.commit('product/setProduct', defaultProduct)
+    store.commit('products/upsertProducts', [{ product: defaultProduct }])
 
-    expect(store.state.product.product.id)
-      .toEqual(defaultProduct.id)
+    expect(store.state.products.products[0].product.id).toEqual(
+      defaultProduct.id
+    )
   })
 
   it('gets first variant if no variant selected', () => {
     const store = new Vuex.Store(storeConfig())
     store.commit('product/setProduct', defaultProduct)
 
-    expect(store.getters['product/currentVariant'].id)
-      .toEqual(defaultProduct.variants[0].id)
+    expect(store.getters['product/currentVariant'].id).toEqual(
+      defaultProduct.variants[0].id
+    )
   })
 
   it('sets the selected variant', () => {
@@ -32,18 +34,17 @@ describe('Event Store', () => {
     store.commit('product/setProduct', defaultProduct)
     store.commit('product/setSelectedVariant', variant)
 
-    expect(store.state.product.selectedVariant.id)
-      .toEqual(variant.id)
+    expect(store.state.product.selectedVariant.id).toEqual(variant.id)
   })
 
   it('sets the selected variant by id', () => {
     const store = new Vuex.Store(storeConfig())
-    const variantId = 'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yODU2ODgyMDAyMzQwMQ=='
+    const variantId =
+      'Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8yODU2ODgyMDAyMzQwMQ=='
 
     store.commit('product/setProduct', defaultProduct)
     store.commit('product/setSelectedVariantById', variantId)
 
-    expect(store.state.product.selectedVariant.id)
-      .toEqual(variantId)
+    expect(store.state.product.selectedVariant.id).toEqual(variantId)
   })
 })
