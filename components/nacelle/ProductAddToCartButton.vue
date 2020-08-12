7e7e001cfb6a79bd43e90c0829e9c9735a32c41d
curbol <curtis.bollinger@gmail.com>
2020-08-12 16:17:38 -0500

    refactored product add to cart button

--- components/nacelle/ProductAddToCartButton.vue
+++ components/nacelle/ProductAddToCartButton.vue
@@ -6,31 +6,9 @@
       @click="addToCart"
       class="add-to-cart-button button is-primary nacelle"
     >
-      <span
-        v-if="
-          !variantInLineItems && !allOptionsSelected && product.availableForSale
-        "
-        >Select Options</span
-      >
-      <span
-        v-if="
-          (!variantInLineItems && allOptionsSelected && variant == undefined) ||
-            (!variantInLineItems &&
-              allOptionsSelected &&
-              variant.availableForSale === false) ||
-            !product.availableForSale
-        "
-        >Out of Stock</span
-      >
-      <span
-        v-if="
-          !variantInLineItems &&
-            allOptionsSelected &&
-            variant &&
-            variant.availableForSale == true
-        "
-        >Add to Cart</span
-      >
+      <span v-if="showSelectOptions">Select Options</span>
+      <span v-if="showOutOfStock">Out of Stock</span>
+      <span v-if="showAddToCart">Add to Cart</span>
       <span v-if="variantInLineItems">Added!</span>
     </button>
     <button
@@ -40,29 +18,19 @@
       :disabled="disableAtcButton"
     >
       <slot>
-        <span v-if="!onlyOneOption && product.availableForSale"
-          >Select Options</span
-        >
+        <span v-if="!onlyOneOption && product.availableForSale">
+          Select Options
+        </span>
         <span
           v-if="
             onlyOneOption &&
               !variantInLineItems &&
               variant.availableForSale == true
           "
-          >Add to Cart</span
         >
-        <span
-          v-if="
-            (!variantInLineItems &&
-              allOptionsSelected &&
-              variant == undefined) ||
-              (!variantInLineItems &&
-                allOptionsSelected &&
-                variant.availableForSale === false) ||
-              !product.availableForSale
-          "
-          >Out of Stock</span
-        >
+          Add to Cart
+        </span>
+        <span v-if="showOutOfStock">Out of Stock</span>
         <span v-if="onlyOneOption && variantInLineItems">Added!</span>
       </slot>
     </button>
@@ -74,11 +42,9 @@ import { mapState, mapActions, mapMutations, mapGetters } from 'vuex'
 
 export default {
   props: {
-    product: {
-      type: Object
-    },
-    variant: {
-      type: Object
+    productHandle: {
+      type: String,
+      default: ''
     },
     metafields: {
       type: Array,
@@ -95,25 +61,33 @@ export default {
 
   computed: {
     ...mapState('cart', ['lineItems']),
+    ...mapGetters('products', ['getProduct', 'getSelectedVariant']),
+
+    product() {
+      return this.getProduct(this.productHandle)
+    },
+
+    variant() {
+      return this.getSelectedVariant(this.productHandle)
+    },
 
     variantInLineItems() {
-      const vm = this
-      if (vm.variant != null) {
-        const lineItem = vm.lineItems.findIndex(lineItem => {
-          return lineItem.variant.id === vm.variant.id
-        })
-        if (lineItem !== -1) {
-          return true
-        } else {
-          return false
-        }
-      } else {
-        return false
-      }
+      return (
+        !!this.variant &&
+        this.lineItems.map(l => l.variant.id).includes(this.variant.id)
+      )
     },
 
     isProductVariantSelectChild() {
       return this.$parent.$options._componentTag === 'product-variant-select'
+    },
+
+    showSelectOptions() {
+      return (
+        !this.variantInLineItems &&
+        !this.allOptionsSelected &&
+        this.product.availableForSale
+      )
     },
 
     disableAtcButton() {
@@ -122,7 +96,25 @@ export default {
         (this.allOptionsSelected && this.variant === undefined) ||
         (!this.variantInLineItems &&
           this.allOptionsSelected &&
-          this.variant.availableForSale !== true)
+          !this.variant.availableForSale)
+      )
+    },
+
+    showOutOfStock() {
+      return (
+        (!this.variantInLineItems &&
+          this.allOptionsSelected &&
+          (!this.variant || !this.variant.availableForSale)) ||
+        !this.product.availableForSale
+      )
+    },
+
+    showAddToCart() {
+      return (
+        !this.variantInLineItems &&
+        this.allOptionsSelected &&
+        this.variant &&
+        this.variant.availableForSale
       )
     }
   },
@@ -140,7 +132,9 @@ export default {
       'incrementLineItem',
       'decrementLineItem'
     ]),
+
     ...mapMutations('cart', ['showCart']),
+
     addToCart() {
       if (this.allOptionsSelected && this.product.availableForSale) {
         const lineItem = {
--- components/nacelle/ProductCard.vue
+++ components/nacelle/ProductCard.vue
@@ -16,8 +16,7 @@
       />
       <product-add-to-cart-button
         v-if="showAddToCart === true"
-        :product="product"
-        :variant="currentVariant"
+        :productHandle="productHandle"
         :allOptionsSelected="allOptionsSelected"
         :confirmedSelection="confirmedSelection"
         @click.native="handleAddToCartClick"
