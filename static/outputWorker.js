6335a429f1c78a662c57269fd928dfc67deced2e
Kristenn Quemener <kquemener1@gmail.com>
2020-10-01 09:44:58 -0500

    Update outputWorker.js: float  comparisons

--- static/outputWorker.js
+++ static/outputWorker.js
@@ -5,18 +5,25 @@ onmessage = function(e) {
   output = output.filter(item => {
     if (vm.activePriceRange) {
       if (vm.activePriceRange.range[0] === 0) {
-        if (parseFloat(item.minPrice) < vm.activePriceRange.range[1]) {
+        if (
+          parseFloat(item.minPrice) < parseFloat(vm.activePriceRange.range[1])
+        ) {
           return true
         } else {
           return false
         }
       } else if (vm.activePriceRange.range[1] === 0) {
-        if (parseFloat(item.minPrice) > vm.activePriceRange.range[0]) {
+        if (
+          parseFloat(item.minPrice) > parseFloat(vm.activePriceRange.range[0])
+        ) {
           return true
         } else {
           return false
         }
-      } else if (parseFloat(item.minPrice) > vm.activePriceRange.range[0] && parseFloat(item.minPrice) < vm.activePriceRange.range[1]) {
+      } else if (
+        parseFloat(item.minPrice) > parseFloat(vm.activePriceRange.range[0]) &&
+        parseFloat(item.minPrice) < parseFloat(vm.activePriceRange.range[1])
+      ) {
         return true
       } else {
         return false
@@ -28,28 +35,32 @@ onmessage = function(e) {
 
   switch (vm.sortBy) {
     case 'hi-low':
-      postMessage(output.sort((a, b) => {
-        if (a.priceRange.min < b.priceRange.min) {
-          return 1
-        }
-        if (a.priceRange.min > b.priceRange.min) {
-          return -1
-        }
+      postMessage(
+        output.sort((a, b) => {
+          if (parseFloat(a.priceRange.min) < parseFloat(b.priceRange.min)) {
+            return 1
+          }
+          if (parseFloat(a.priceRange.min) > parseFloat(b.priceRange.min)) {
+            return -1
+          }
 
-        return 0
-      }))
+          return 0
+        })
+      )
       break
     case 'low-hi':
-      postMessage(output.sort((a, b) => {
-        if (a.priceRange.min < b.priceRange.min) {
-          return -1
-        }
-        if (a.priceRange.min > b.priceRange.min) {
-          return 1
-        }
+      postMessage(
+        output.sort((a, b) => {
+          if (parseFloat(a.priceRange.min) < parseFloat(b.priceRange.min)) {
+            return -1
+          }
+          if (parseFloat(a.priceRange.min) > parseFloat(b.priceRange.min)) {
+            return 1
+          }
 
-        return 0
-      }))
+          return 0
+        })
+      )
       break
     default:
       postMessage(output)
