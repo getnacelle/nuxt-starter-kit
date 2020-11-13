a71772950b4786f5da98db6dc0271685597c2106
jongeyer <jon@supraset.com>
2020-11-13 14:05:44 -0800

    modify selected options until variant is found

--- components/nacelle/ProductVariantSelect.vue
+++ components/nacelle/ProductVariantSelect.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="variant-select nacelle">
     <product-options
-    v-if="product.variants.length > 1"
-          :selectedVariant="selectedVariant"
+      v-if="product.variants.length > 1"
+      :selectedVariant="selectedVariant"
       :variants="product.variants"
-      :productId="product.pimSyncSourceProductId"
+      :productId="pimId"
     />
     <slot name="above-button"></slot>
     <div class="columns is-mobile">
@@ -23,12 +23,19 @@
 </template>
 
 <script>
+// import { mapGetters, mapMutations } from 'vuex'
 import { mapGetters, mapMutations } from 'vuex'
+
 import ProductOptions from '~/components/nacelle/ProductOptions'
 import QuantitySelector from '~/components/nacelle/QuantitySelector'
 import ProductAddToCartButton from '~/components/nacelle/ProductAddToCartButton'
 
 export default {
+  components: {
+    ProductOptions,
+    QuantitySelector,
+    ProductAddToCartButton
+  },
   props: {
     showQuantitySelect: {
       type: Boolean,
@@ -43,20 +50,30 @@ export default {
       quantity: 1
     }
   },
+  computed: {
+    pimId() {
+      return this.product.pimSyncSourceProductId
+    },
+    selectedVariant() {
+      return this.$store.getters[`product/${this.pimId}/selectedVariant`]
+    }
+  },
   created() {
-    const vm = this
-    if (!this.$store.hasModule(this.product.pimSyncSourceProductId)) {
-      this.$store.registerModule(this.product.pimSyncSourceProductId, {
+    // createProductModule(this.product)
+
+    const { product } = this
+    if (!this.$store.hasModule(['product', this.pimId])) {
+      this.$store.registerModule(['product', this.pimId], {
         state: () => {
           return { selectedOptions: [] }
         },
         getters: {
           selectedVariant: (state) => {
             if (state.selectedOptions.length === 0) {
-              return vm.product.variants[0]
+              return product.variants[0]
             } else {
-              return vm.product.variants.find((variant) => {
-                return state.selectedOptions.every((option) => {
+              return product.variants.find(variant => {
+                return state.selectedOptions.every(option => {
                   return variant.selectedOptions.some(variantOption => JSON.stringify(variantOption) === JSON.stringify(option))
                 })
               })
@@ -68,7 +85,6 @@ export default {
             if (state.selectedOptions.length > 0) {
               const index = state.selectedOptions.findIndex((item) => item.name === selectedOption.name)
               if (index > -1) {
-                console.log(index)
                 state.selectedOptions[index].value = selectedOption.value
               } else {
                 state.selectedOptions.push(selectedOption)
@@ -76,25 +92,24 @@ export default {
             } else {
               state.selectedOptions.push(selectedOption)
             }
+
+            // if `selectedOptions` does not match any variant
+            // then select remove options until matching variant based on `selectedOption`
+            const findSelectedVariant = () => product.variants.find(variant => {
+              return state.selectedOptions.every(option => {
+                return variant.selectedOptions.some(variantOption => JSON.stringify(variantOption) === JSON.stringify(option))
+              })
+            })
+            let selectedVariant = findSelectedVariant()
+            while (!selectedVariant && state.selectedOptions.length > 0) {
+              state.selectedOptions.shift()
+              selectedVariant = findSelectedVariant()
+            }
           }
         },
         namespaced: true
       })
     }
-  },
-  computed: {
-    selectedVariant() {
-      if (this.$store.getters[[`${this.product.pimSyncSourceProductId}/selectedVariant`]]) {
-        return this.$store.getters[`${this.product.pimSyncSourceProductId}/selectedVariant`]
-      }
-      return null
-    }
-  },
-  components: {
-    ProductOptions,
-    QuantitySelector,
-    ProductAddToCartButton
-
   }
 }
 </script>
