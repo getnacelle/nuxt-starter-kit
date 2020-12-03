c0421ebee6a6c482f911e01d83efd077b8c0269d
Levi Whalen <levi@leviwhalen.com>
2020-12-02 22:02:43 -0800

    setting collection.products outside fetchProducts() seems to work consistently

---
+++ assets/nacelle-routing/getData.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import dotenv from 'dotenv'
+
+const spaceID = process.env.NACELLE_SPACE_ID
+const token = process.env.NACELLE_GRAPHQL_TOKEN
+const version = process.env.NACELLE_API_VERSION
+
+dotenv.config()
+
+export default async query => {
+  const res = await axios({
+    method: 'post',
+    url: `https://hailfrequency.com/${version}/graphql`,
+    headers: {
+      'x-nacelle-space-id': spaceID,
+      'x-nacelle-space-token': token,
+      'Content-Type': 'application/json'
+    },
+    data: {
+      query: query
+    }
+  })
+  return res.data.data
+}
