c0e67b506b34b83589c1184df59c079f6cbd839c
Nick Richmond <get@nickrichmond.dev>
2021-02-17 14:58:20 -0500

    use lightweight http client; handle errors

--- assets/nacelle-routing/getData.js
+++ assets/nacelle-routing/getData.js
@@ -1,22 +1,27 @@
-import axios from 'axios'
 import dotenv from 'dotenv'
+import fetch from 'isomorphic-unfetch'
+
+dotenv.config()
 
 const spaceID = process.env.NACELLE_SPACE_ID
 const token = process.env.NACELLE_GRAPHQL_TOKEN
 const version = process.env.NACELLE_API_VERSION || 'v2'
-
-dotenv.config()
+console.log(`TOKEN: ${token}`)
 
 export default async (query) => {
-  const res = await axios({
-    method: 'post',
-    url: `https://hailfrequency.com/${version}/graphql`,
-    headers: {
-      'x-nacelle-space-id': spaceID,
-      'x-nacelle-space-token': token,
-      'Content-Type': 'application/json'
-    },
-    data: { query }
-  })
-  return res.data.data
+  try {
+    const res = await fetch(`https://hailfrequency.com/${version}/graphql`, {
+      headers: {
+        'x-nacelle-space-id': spaceID,
+        'x-nacelle-space-token': token,
+        'Content-Type': 'application/json'
+      },
+      body: { query }
+    }).then((res) => res.json())
+
+    return res.data
+  } catch (err) {
+    console.warn(`RUH ROH!`)
+    throw new Error(err)
+  }
 }
