a371d35e191cfa0f86d9b556e288e3923c0019f2
Levi Whalen <levi@leviwhalen.com>
2021-02-18 14:21:27 -0800

    returning null from fetch() wherepages are not found

--- pages/pages/_pageHandle.vue
+++ pages/pages/_pageHandle.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="page">
     <page-content v-if="!$fetchState.pending" :page="page" />
+    <nacelle-page-placeholder
+      v-if="this.$fetchState.pending === false && page === null"
+    />
   </div>
 </template>
 
@@ -13,9 +16,10 @@ export default {
   },
   async fetch() {
     const { pageHandle: handle } = this.$route.params
-    this.page = await this.$nacelle.data
-      .page({ handle })
-      .catch(() => console.warn(`No page with handle: '${handle}' found`))
+    this.page = await this.$nacelle.data.page({ handle }).catch(() => {
+      console.warn(`No page with handle: '${handle}' found`)
+      return null
+    })
   }
 }
 </script>
