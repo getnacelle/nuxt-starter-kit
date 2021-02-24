a215f3b2bd40e547e2b66b66c00f9213c6b74a10
Levi Whalen <levi@leviwhalen.com>
2021-02-24 12:18:43 -0800

    try catch

--- modules/dollar-nacelle.js
+++ modules/dollar-nacelle.js
@@ -1,61 +1,59 @@
 import NacelleClient from '@nacelle/client-js-sdk/dist/client-js-sdk.esm'
 
 export default function (context, inject) {
+  try {
+    const { settings, space } = JSON.parse(`<%= JSON.stringify(options) %>`)
+    const {
+      spaceID,
+      token,
+      endpoint,
+      tem,
+      wishlistEndpoint,
+      defaultLocale
+    } = settings
 
-<%= typeof options!=='undefined' ? options : '' %>
+    const client = new NacelleClient({
+      id: spaceID,
+      token,
+      nacelleEndpoint: endpoint,
+      locale: defaultLocale,
+      eventsEndpoint: tem,
+      wishlistEndpoint,
+      useStatic: false
+    })
 
+    const setSpace = () => {
+      const { commit } = context.store
 
-  const { settings, space } = <%= JSON.stringify(options) %> // eslint-disable-line
-
-  console.log(space)
-  const { 
-    spaceID,
-    token,
-    endpoint,
-    tem,
-    wishlistEndpoint,
-    defaultLocale
-  } = settings
-
-  const client = new NacelleClient({
-    id: spaceID,
-    token,
-    nacelleEndpoint: endpoint,
-    locale: defaultLocale,
-    eventsEndpoint: tem,
-    wishlistEndpoint,
-    useStatic: false
-  })
-
-  const setSpace = () => {
-    const { commit } = context.store
-
-    if (space) {
-      const { id, name, domain, metafields, linklists } = space
-      commit('space/setId', id)
-      commit('space/setName', name)
-      commit('space/setDomain', domain)
-      commit('space/setMetafields', metafields)
-      commit('space/setLinklists', linklists)
+      if (space) {
+        const { id, name, domain, metafields, linklists } = space
+        commit('space/setId', id)
+        commit('space/setName', name)
+        commit('space/setDomain', domain)
+        commit('space/setMetafields', metafields)
+        commit('space/setLinklists', linklists)
+      }
     }
-  }
 
-  const nacelleNuxtServerInit = () => {
-    setSpace()
-  }
+    const nacelleNuxtServerInit = () => {
+      setSpace()
+    }
 
-  const plugin = {
-    ...settings,
-    defaultLocale,
-    nacelleNuxtServerInit,
-    setSpace,
-    client,
-    data: client.data,
-    checkout: client.checkout,
-    events: client.events,
-    status: client.status,
-    wishlist: client.wishlist
-  }
+    const plugin = {
+      ...settings,
+      defaultLocale,
+      nacelleNuxtServerInit,
+      setSpace,
+      client,
+      data: client.data,
+      checkout: client.checkout,
+      events: client.events,
+      status: client.status,
+      wishlist: client.wishlist
+    }
 
-  inject('nacelle', plugin)
+    inject('nacelle', plugin)
+  } catch (err) {
+    return null
+  }
 }
